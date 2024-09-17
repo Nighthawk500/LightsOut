fbe77070aca164201807eebac8ff9ada74b150cd
Nighthawk500 <jafm0108@gmail.com>
2024-09-17 16:29:52 -0400

    help button functionality

--- app/src/main/java/com/zybooks/lightsout/MainActivity.java
+++ app/src/main/java/com/zybooks/lightsout/MainActivity.java
@@ -1,5 +1,6 @@
 package com.zybooks.lightsout;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -70,6 +71,11 @@ public class MainActivity extends AppCompatActivity {
     private void startGame() {
         mGame.newGame();
         setButtonColors();
+    }
+
+    public void onHelpClick(View view) {
+        Intent intent = new Intent(this, HelpActivity.class);
+        startActivity(intent);
     }
 
     private void onLightButtonClick(View view) {
