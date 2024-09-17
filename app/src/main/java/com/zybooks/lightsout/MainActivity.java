c693f064fb40abd9f8363755409965c5433dce5a
Nighthawk500 <jafm0108@gmail.com>
2024-09-17 17:07:47 -0400

    button color change functionality

--- app/src/main/java/com/zybooks/lightsout/MainActivity.java
+++ app/src/main/java/com/zybooks/lightsout/MainActivity.java
@@ -1,5 +1,6 @@
 package com.zybooks.lightsout;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -7,6 +8,10 @@ import android.widget.Button;
 import android.widget.GridLayout;
 import android.widget.Toast;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
@@ -20,11 +25,14 @@ public class MainActivity extends AppCompatActivity {
     private int mLightOffColor;
     private String mLightOnContent;
     private String mLightOffContent;
+    private int mLightOnColorId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mLightOnColorId = R.color.yellow;
 
         mLightGrid = findViewById(R.id.light_grid);
 
@@ -116,4 +124,28 @@ public class MainActivity extends AppCompatActivity {
     public void onNewGameClick(View view) {
         startGame();
     }
+
+    public void onChangeColorClick(View view) {
+        // Send current color ID to ColorActivity
+        Intent intent = new Intent(this, ColorActivity.class);
+        intent.putExtra(ColorActivity.EXTRA_COLOR, mLightOnColorId);
+        mColorResultLauncher.launch(intent);
+    }
+
+    private final ActivityResultLauncher<Intent> mColorResultLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        Intent data = result.getData();
+                        if (data != null) {
+                            //Create the on button color from the chosen color ID from ColorActivity
+                            mLightOnColorId = data.getIntExtra(ColorActivity.EXTRA_COLOR, R.color.yellow);
+                            mLightOnColor = ContextCompat.getColor(MainActivity.this, mLightOnColorId);
+                            setButtonColors();
+                        }
+                    }
+                }
+            });
 }
